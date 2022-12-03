e5afd5459a0f206ed56f9e8e660ec8cb3b18174e
everdimension <everdimension@gmail.com>
2022-12-03 15:42:47 +0700

    Setup testing env, add tests

---
+++ babel.config.cjs
@@ -0,0 +1,9 @@
+/** Babel is used only for tests which are run by jest */
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+};
---
+++ tests/keyboard-shortcut.test.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { KeyboardShortcut } from '../src/index';
+
+test('KeyboardShortcut fires correctly', async () => {
+  const handler = jest.fn();
+  render(<KeyboardShortcut combination="shift+w" onKeyDown={handler} />);
+
+  const wrongEvent = {
+    shiftKey: true,
+    which: 69, // code for "e"
+  };
+
+  const shiftWEvent = {
+    shiftKey: true,
+    which: 87, // code for "w"
+  };
+
+  fireEvent.keyDown(document, wrongEvent);
+  expect(handler).toHaveBeenCalledTimes(0); // should not have been called yet
+
+  fireEvent.keyDown(document, shiftWEvent);
+  expect(handler).toHaveBeenCalled();
+  expect(handler).toHaveBeenCalledTimes(1);
+});
+
+test('KeyboardShortcut removes listeners when unmounted', async () => {
+  const handler = jest.fn();
+  const { unmount } = render(
+    <KeyboardShortcut combination="shift+w" onKeyDown={handler} />
+  );
+  const shiftWEvent = {
+    shiftKey: true,
+    which: 87, // code for "w"
+  };
+  fireEvent.keyDown(document, shiftWEvent);
+  expect(handler).toHaveBeenCalled();
+  fireEvent.keyDown(document, shiftWEvent);
+  expect(handler).toHaveBeenCalledTimes(2);
+
+  unmount()
+  fireEvent.keyDown(document, shiftWEvent);
+  expect(handler).toHaveBeenCalledTimes(2);
+});
