5c74646d2b73e3e0e7e7495634197276a8a7ab8a
everdimension <everdimension@gmail.com>
2022-12-05 12:41:08 +0700

    Refactor and update tests

--- tests/keyboard-shortcut.test.tsx
+++ tests/keyboard-shortcut.test.tsx
@@ -3,58 +3,69 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { KeyboardShortcut } from '../src/index';
 
-test('KeyboardShortcut fires correctly', async () => {
-  const handler = jest.fn();
-  render(<KeyboardShortcut combination="shift+w" onKeyDown={handler} />);
+const keyboardEvents = {
+  esc: { which: 27 },
+  w: { which: 87 },
+  'shift+w': { shiftKey: true, which: 87 },
+  'shift+e': { shiftKey: true, which: 69 },
+};
 
-  const wrongEvent = {
-    shiftKey: true,
-    which: 69, // code for "e"
-  };
+describe('KeyboardShortcut', () => {
+  test('fires callbacks correctly for key combinations', async () => {
+    const handler = jest.fn();
+    render(<KeyboardShortcut combination="shift+w" onKeyDown={handler} />);
 
-  const shiftWEvent = {
-    shiftKey: true,
-    which: 87, // code for "w"
-  };
+    const wrongEvent = keyboardEvents['shift+e'];
+    const expectedEvent = keyboardEvents['shift+w'];
 
-  fireEvent.keyDown(document, wrongEvent);
-  expect(handler).toHaveBeenCalledTimes(0); // should not have been called yet
+    fireEvent.keyDown(document, wrongEvent);
+    expect(handler).toHaveBeenCalledTimes(0); // should not have been called yet
 
-  fireEvent.keyDown(document, shiftWEvent);
-  expect(handler).toHaveBeenCalled();
-  expect(handler).toHaveBeenCalledTimes(1);
-});
+    fireEvent.keyDown(document, expectedEvent);
+    expect(handler).toHaveBeenCalled();
+    expect(handler).toHaveBeenCalledTimes(1);
+  });
 
-test('KeyboardShortcut handles single keys', async () => {
-  const handler = jest.fn();
-  render(<KeyboardShortcut combination="esc" onKeyDown={handler} />);
+  test('handles single key combinations', async () => {
+    const handlerEsc = jest.fn();
+    const handlerW = jest.fn();
+    render(<KeyboardShortcut combination="esc" onKeyDown={handlerEsc} />);
+    render(<KeyboardShortcut combination="w" onKeyDown={handlerW} />);
 
-  const escapeKeyEvent = {
-    which: 27, // code for "Esc"
-  };
+    // const escapeKeyEvent = {
+    //   which: 27, // code for "Esc"
+    // };
+    // const wKeyEvent = {
+    //   which: 27, // code for "Esc"
+    // };
 
-  expect(handler).toHaveBeenCalledTimes(0); // should not have been called yet
+    expect(handlerEsc).toHaveBeenCalledTimes(0); // should not have been called yet
+    expect(handlerW).toHaveBeenCalledTimes(0); // should not have been called yet
 
-  fireEvent.keyDown(document, escapeKeyEvent);
-  expect(handler).toHaveBeenCalled();
-  expect(handler).toHaveBeenCalledTimes(1);
-});
+    fireEvent.keyDown(document, keyboardEvents.esc);
+    expect(handlerEsc).toHaveBeenCalled();
+    expect(handlerEsc).toHaveBeenCalledTimes(1);
+    expect(handlerW).toHaveBeenCalledTimes(0); // should not have been called yet
 
-test('KeyboardShortcut removes listeners when unmounted', async () => {
-  const handler = jest.fn();
-  const { unmount } = render(
-    <KeyboardShortcut combination="shift+w" onKeyDown={handler} />
-  );
-  const shiftWEvent = {
-    shiftKey: true,
-    which: 87, // code for "w"
-  };
-  fireEvent.keyDown(document, shiftWEvent);
-  expect(handler).toHaveBeenCalled();
-  fireEvent.keyDown(document, shiftWEvent);
-  expect(handler).toHaveBeenCalledTimes(2);
+    fireEvent.keyDown(document, keyboardEvents.w);
+    expect(handlerW).toHaveBeenCalled();
+    expect(handlerW).toHaveBeenCalledTimes(1);
+  });
 
-  unmount();
-  fireEvent.keyDown(document, shiftWEvent);
-  expect(handler).toHaveBeenCalledTimes(2);
+  test('removes listeners when unmounted', async () => {
+    const handler = jest.fn();
+    const { unmount } = render(
+      <KeyboardShortcut combination="shift+w" onKeyDown={handler} />
+    );
+    const keyboardEvent = keyboardEvents['shift+w'];
+
+    fireEvent.keyDown(document, keyboardEvent);
+    expect(handler).toHaveBeenCalled();
+    fireEvent.keyDown(document, keyboardEvent);
+    expect(handler).toHaveBeenCalledTimes(2);
+
+    unmount();
+    fireEvent.keyDown(document, keyboardEvent);
+    expect(handler).toHaveBeenCalledTimes(2);
+  });
 });
